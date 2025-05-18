0513bf91e8cb9ad813eab356ce22d48065ade4cb
ruby <bigdipper201201@gmail.com>
2025-05-18 19:03:19 +0700

    Revert "feat: abc"
    This reverts commit 5bee25379a22c56cb7c34920c4224ded52658733.

--- src/steps/loginSteps.ts
+++
@@ -1,6 +0,0 @@
-async function testCode(name:string) : Promise<void> {
-    let greeting : string = 'hello';
-    let array : Array<string[]> = [];
-        console.log('greeting',greeting);
-        
-}

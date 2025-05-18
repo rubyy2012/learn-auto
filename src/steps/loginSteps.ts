5bee25379a22c56cb7c34920c4224ded52658733
ruby <bigdipper201201@gmail.com>
2025-05-18 18:51:00 +0700

    feat: abc

---
+++ src/steps/loginSteps.ts
@@ -0,0 +1,6 @@
+async function testCode(name:string) : Promise<void> {
+    let greeting : string = 'hello';
+    let array : Array<string[]> = [];
+        console.log('greeting',greeting);
+        
+}
